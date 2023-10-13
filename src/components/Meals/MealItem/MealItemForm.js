4c4ff3e875fdd1711f492efbe1203fc857b070c3
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-10-13 05:50:36 +0200

    added few basic components

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './MealItemForm.module.css'
+
+const MealItemForm = (props) =>{
+    return <form className={styles.form}>
+        <input />
+        <button>+ Add</button>
+    </form>
+}
+export default MealItemForm;
