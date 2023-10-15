a6c9e22463b07bd7ecdd72db1f70f72df594b494
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-10-15 06:05:40 +0200

    added add and remove handlers and useEffect animation on header

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -6,12 +6,26 @@ import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
+  const {items} = cartCtx
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`
+
+  useEffect(()=>{
+    if(items.length ===0){
+      return;
+    }
+    setBtnIsHighlighted(true)
+
+    setTimeout(()=>{
+      setBtnIsHighlighted(false);
+    }, 300);
+  }, [items])
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
