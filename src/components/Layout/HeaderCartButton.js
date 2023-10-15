e93ed8d63fcbb8b409b8ba0d083c6e9c98cef86e
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2023-10-15 06:09:39 +0200

    fixed the timer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -20,9 +20,13 @@ const HeaderCartButton = (props) => {
     }
     setBtnIsHighlighted(true)
 
-    setTimeout(()=>{
+    const timer = setTimeout(()=>{
       setBtnIsHighlighted(false);
     }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
   }, [items])
   return (
     <button className={btnStyles} onClick={props.onClick}>
